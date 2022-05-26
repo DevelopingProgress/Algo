2b0184f35bbfa263f22845b3b4c6b04cdc038250
DevelopingProgress <kacpgaw020@student.polsl.pl>
2022-05-26 21:13:30 +0200

    Added rest of algos to transform

--- 5/hare.py
+++ 5/hare.py
@@ -1,7 +1,49 @@
-def hare(votes, seats):
-    return 1.0 * votes / seats
+import math
+
+votes = [720, 300, 480]
+distribution = []
+calc_tab = []
+max_index = 0
+num_committees = len(votes)
+num_seats = 8
 
 
-votes = 750
-seats = 80
-print(hare(votes, seats))
+def totalVotes(votes):
+    votes_count = 0
+    for i in range(num_committees):
+        votes_count += votes[i]
+    return votes_count
+
+
+def mandates(votes):
+    mandates_count = 0
+    votes_count = totalVotes(votes)
+    calc_tab.clear()
+    for i in range(num_committees):
+        calc_tab.append((votes[i] * num_seats * 1.0) / votes_count)
+        distribution.append(int(math.floor(calc_tab[i])))
+        calc_tab[i] -= distribution[i]
+        mandates_count += distribution[i]
+    return mandates_count
+
+
+def rest_mandates(votes):
+    mandates_count = mandates(votes)
+    while mandates_count < num_seats:
+        max = -1
+        for i in range(num_committees):
+            if max < calc_tab[i]:
+                max = calc_tab[i]
+                max_index = i
+        distribution[max_index] += 1
+        mandates_count += 1
+        calc_tab[max_index] = 0
+    return distribution
+
+
+def display(distribution):
+    for i in range(num_committees):
+        print("Committee", (i + 1), distribution[i])
+
+
+display(rest_mandates(votes))
--- 5/sainte_lague.py
+++ 5/sainte_lague.py
@@ -1,14 +1,37 @@
-def normalize(values):
-    total = sum(values)
-    return [1.0 * value / total for value in values]
+import math
+
+votes = [720, 300, 480]
+distribution = []
+calc_tab = []
+max_index = 0
+num_committees = len(votes)
+num_seats = 8
+
+def calculate(votes, number_seats):
+    return (votes * 1.0) / (2 * number_seats + 1.0)
+
+def sainte_lague(votes):
+    calc_tab.clear()
+    for i in range(num_committees):
+        distribution.append(0)
+
+    for i in range(num_committees):
+        calc_tab.append(votes[i])
+
+    for i in range(num_seats):
+        maks = -1
+        for j in range(num_committees):
+            if maks < calc_tab[j]:
+                maks = calc_tab[j]
+                max_index = j
+        distribution[max_index] += 1
+        calc_tab[max_index] = calculate(votes[max_index], distribution[max_index])
+    return distribution
 
 
-def sainte_lague(votes, seats):
-    votes = normalize(votes)
-    seats = normalize(seats)
-    return sum(1.0 / v * (v - s) ** 2 for v, s in zip(votes, seats))
+def display(distribution):
+    for i in range(num_committees):
+        print("Committee", (i + 1), distribution[i])
 
 
-votes = [750, 150, 50, 50]
-seats = [80, 16, 2, 2]
-print(sainte_lague(votes, seats))
+display(sainte_lague(votes))
